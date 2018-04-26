1b877593c3c31dddb61f7686b51a5ef5cafaec18
etmPlatform <5329422@qq.com>
2018-04-26 14:18:56 +0800

    format files

--- framework/helpers/base58check/bs58.js
+++ framework/helpers/base58check/bs58.js
@@ -1,1 +1,4 @@
-"use strict";var basex=require("./base-x.js"),ALPHABET="123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";module.exports=basex(ALPHABET);
+"use strict";
+var basex = require("./base-x.js"),
+ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+module.exports = basex(ALPHABET);
