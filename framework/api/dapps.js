605f2f58122f04717d9769c112931a16ac550e98
just-anohaker <just.anohaker@gmail.com>
2019-07-15 16:11:57 +0800

    添加Dapp自定义通知功能

--- framework/api/dapps.js
+++ framework/api/dapps.js
@@ -1,79 +1,87 @@
 "use strict";
 var self = null,
-library = null,
-modules = null;
+    library = null,
+    modules = null;
 function Dapps(a, s) {
     library = s,
-    a(null, self = this)
+        a(null, self = this)
 }
-Dapps.prototype.getGenesis = function(a) {
+
+Dapps.prototype.notification = function (notification, cb) {
+    library.sandbox.sendMessage({
+        call: "dapps#notification",
+        args: notification || {}
+    }, cb);
+}
+
+Dapps.prototype.getGenesis = function (a) {
     library.sandbox.sendMessage({
         call: "dapps#getGenesis",
         args: {}
     },
-    a)
+        a)
 },
-Dapps.prototype.sendWithdrawal = function(a, s) {
-    var e = {
-        call: "dapps#sendWithdrawal",
-        args: a
-    };
-    library.sandbox.sendMessage(e, s)
-},
-Dapps.prototype.getCommonBlock = function(a) {
-    library.sandbox.sendMessage({
-        call: "dapps#getCommonBlock",
-        args: {}
+    Dapps.prototype.sendWithdrawal = function (a, s) {
+        var e = {
+            call: "dapps#sendWithdrawal",
+            args: a
+        };
+        library.sandbox.sendMessage(e, s)
     },
-    a)
-},
-Dapps.prototype.setReady = function(a) {
-    library.sandbox.sendMessage({
-        call: "dapps#setReady",
-        args: {}
+    Dapps.prototype.getCommonBlock = function (a) {
+        library.sandbox.sendMessage({
+            call: "dapps#getCommonBlock",
+            args: {}
+        },
+            a)
     },
-    a)
-},
-Dapps.prototype.getWithdrawalLastTransaction = function(a) {
-    library.sandbox.sendMessage({
-        call: "dapps#getWithdrawalLastTransaction",
-        args: {}
+    Dapps.prototype.setReady = function (a) {
+        library.sandbox.sendMessage({
+            call: "dapps#setReady",
+            args: {}
+        },
+            a)
     },
-    a)
-},
-Dapps.prototype.getBalanceTransactions = function(a, s) {
-    var e = {
-        call: "dapps#getBalanceTransactions",
-        args: {
-            lastTransactionId: a
-        }
-    };
-    library.sandbox.sendMessage(e, s)
-},
-Dapps.prototype.getDApp = function(a) {
-    library.sandbox.sendMessage({
-        call: "dapps#getDApp"
+    Dapps.prototype.getWithdrawalLastTransaction = function (a) {
+        library.sandbox.sendMessage({
+            call: "dapps#getWithdrawalLastTransaction",
+            args: {}
+        },
+            a)
     },
-    a)
-},
-Dapps.prototype.submitOutTransfer = function(a, s) {
-    var e = {
-        call: "dapps#submitOutTransfer",
-        args: a
-    };
-    library.sandbox.sendMessage(e, s)
-},
-Dapps.prototype.registerInterface = function(a, s) {
-    var e = {
-        call: "dapps#registerInterface",
-        args: {
-            method: a.method,
-            path: a.path
-        }
-    };
-    library.sandbox.sendMessage(e, s)
-},
-Dapps.prototype.onBind = function(a) {
-    modules = a
-},
-module.exports = Dapps;
+    Dapps.prototype.getBalanceTransactions = function (a, s) {
+        var e = {
+            call: "dapps#getBalanceTransactions",
+            args: {
+                lastTransactionId: a
+            }
+        };
+        library.sandbox.sendMessage(e, s)
+    },
+    Dapps.prototype.getDApp = function (a) {
+        library.sandbox.sendMessage({
+            call: "dapps#getDApp"
+        },
+            a)
+    },
+    Dapps.prototype.submitOutTransfer = function (a, s) {
+        var e = {
+            call: "dapps#submitOutTransfer",
+            args: a
+        };
+        library.sandbox.sendMessage(e, s)
+    },
+    Dapps.prototype.registerInterface = function (a, s) {
+        var e = {
+            call: "dapps#registerInterface",
+            args: {
+                method: a.method,
+                path: a.path
+            }
+        };
+        library.sandbox.sendMessage(e, s)
+    },
+    Dapps.prototype.onBind = function (a) {
+        modules = a
+    },
+    module.exports = Dapps;
